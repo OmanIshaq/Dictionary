584bf4099bf3a3e6bd24252d1685a92f2333ad17
oman ishaq <omanishaq>
2019-11-24 00:23:29 +0500

    Create Form1.cs

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Tree temp = new Tree();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void LoadDictionary()
+        {
+            string line;
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"E:\New folder\4th semester\DSA\DSA\spell corector\spell corector\WORDS.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                temp.InsertData(line);
+            }
+
+            file.Close();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDictionary();
+
+        }
