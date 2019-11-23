e991627869152e7a7c46ba40676d0e35bb228f22
oman ishaq <omanishaq>
2019-11-24 00:14:16 +0500

    Update node.cs

--- node.cs
+++ node.cs
@@ -8,7 +8,38 @@ namespace WindowsFormsApp1
 {
     public class Node
     {
-       
+        public string data { get; set; }
+        public Node left { get; set; }
+        public Node right { get; set; }
+
+        public Node()
+        {
+            data = "";
+            left = null;
+            right = null;
+        }
+        public Node(string s)
+        {
+            this.data = s;
+        }
+
+    }
+    public class Tree
+    {
+        public Node root;
+        public Tree()
+        {
+            root = null;
+        }
+        public void InsertData(string data)
+        {
+            if (root == null)
+            {
+                root = new Node(data);
+                return;
+            }
+            Insertatchild(root, new Node(data));
+        }
 
     }
 }
